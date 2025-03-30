db9dc83cccaedcee46dab0efbfe5d97b73fd3944
Ayushd785 <ayushd785@gmail.com>
2025-03-31 00:58:12 +0530

    Initial commit

---
+++ app/src/main/java/ticket/booking/entities/Users.java
@@ -0,0 +1,15 @@
+package ticket.booking.entities;
+import java.util.*;
+public class Users {
+    private String name;
+
+    private String password;
+
+    private String hashedPassword;
+
+    private List<Ticket> ticketsBooked;
+
+    private String userId;
+
+
+}
---
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -0,0 +1,14 @@
+package ticket.booking.services;
+
+import ticket.booking.entities.Users;
+
+public class UserBookingService {
+    private Users user;
+
+    public UserBookingService(Users user) {
+        this.user = user;
+
+    }
+
+
+}
